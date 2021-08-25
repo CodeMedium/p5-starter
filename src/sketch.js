863e64bdd3fa11effdedba13bcfa273e80ac1e38
Art See Clarke <thecodemedium@gmail.com>
2021-08-25 14:52:40 -0700

    Adds params to starter

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,34 @@
+/**
+ * Title
+ * Started: 8/22/21
+ * By: Art See Clarke
+ * Twitter: https://twitter.com/thecodemedium
+ * GitHub: https://github.com/codemedium
+ * Personal website: https://codemedium.com
+ *
+ * 		 "Any sufficiently advanced technology is indistinguishable from magic"
+ * 		 - Arthur C. Clarke
+ *
+ * Description: 
+ */
+
+/**
+ * Color palettes
+ */
+// VSCode Shades of purple editor colors
+colors = ['#ffffff', '#ff628c', '#FF9D00', '#fad000', '#2ca300', '#2EC4B6', '#5D37F0']
+
+/**
+ * Sketch entry point
+ */
 function setup() {
+  // Param args
+  params = Object.assign({
+
+  }, getURLParams())
+
 	createCanvas(windowWidth, windowHeight)
-	background(100)
 }
 
 function draw() {
-	ellipse(mouseX, mouseY, 20, 20)
 }
